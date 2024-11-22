0e84c704fcdc51dc472e5c5209002af284d80eab
Zaid Khan <itszaidkhan01@gmail.com>
2024-11-22 04:34:17 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // it is used in browser and currently we are using nodejs
+
+//we can use semicolon or not, but mostly times semicolon is not preferred to use
+
+let name = "Zaid"   //string datatype
+let age = 19   //integer datatype
+let isLoggedIn = false   //boolean datatype
+
+// number => 2 to the power 53 then use 'bigInt'
+// string => can be use in "", or ''. But mostly prefer double quotes ""
+// boolean => true/false
+// null => standAloneValue, it means empty
+// undefined => it means not defined, null is different because null directly tells that it is empty
+// symbol => unique
+
+console.log(typeof "hello"); // gives the type of datatype
+console.log(typeof age); 
+console.log(typeof null);  // from this we get that null is an object
+console.log(typeof undefined); // undefined
